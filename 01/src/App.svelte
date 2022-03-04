3b31785504ad48431de06515774afc2a60128e29
lucnova <nlnovae@gmail.com>
2022-03-03 21:57:02 -0600

    First Hello World in Svelte!

---
+++ 01/src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+	export let name;
+	export let age;
+
+	const incrementAge = () => {
+		age += 1;
+	};
+	const decrementAge = () => {
+		age -= 1;
+	};
+</script>
+
+<h1>Hello {name}, I'm {age} years old.</h1>
+<button on:click={decrementAge}>- 1</button>
+<button on:click={incrementAge}>+ 1</button>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
