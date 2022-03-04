39f56a6929b71f3987091765d89305ebe2a7d124
lucnova <nlnovae@gmail.com>
2022-03-04 00:49:33 -0600

    Data Binding w Inputs

--- 01/src/App.svelte
+++ 01/src/App.svelte
@@ -2,7 +2,7 @@
 	export let name;
 	export let age;
 
-	// * Reactive Variables: Dollar Sign => Equivalente a los efectos de React.
+	// * REACTIVE VARIABLES: Dollar Sign => Equivalente a los efectos de React.
 	// La primer variable indicada del lado derecho es la que hará los cambios automaticamente
 	$: uppercaseName = name.toUpperCase();
 	$: console.log(age);
@@ -13,11 +13,18 @@
 	const decrementAge = () => {
 		age -= 1;
 	};
+
+	const handleNameChange = (e) => {
+		name = e.target.value.trim();
+	};
 </script>
 
 <h1>Hello {uppercaseName}, I'm {age} years old.</h1>
 <button on:click={decrementAge}>- 1</button>
 <button on:click={incrementAge}>+ 1</button>
+<!-- DATA BINDING: Por defecto  solo es una conexión de 1 lado (Salida) -->
+<!-- on:input => realizado cada que se cambie el valor -->
+<input type="text" value={name} on:input={handleNameChange} />
 
 <style>
 	h1 {
