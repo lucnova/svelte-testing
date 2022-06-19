882a626dc2c223aad77cab7a72b62abe593ee769
lucnova <nlnovae@gmail.com>
2022-06-18 20:04:21 -0500

    Excercise Complete

--- 02-assignment-problem/src/App.svelte
+++ 02-assignment-problem/src/App.svelte
@@ -1,10 +1,25 @@
+<script>
+	import CourseGoal from './CourseGoal.svelte';
+
+	export let courseGoal;
+	$: console.log(courseGoal.includes('!'));
+</script>
+
 <h1>Assignment</h1>
 
 <p>Solve these tasks.</p>
 
 <ol>
-	<li>Add an input field that allows users to enter a course goal.</li>
-	<li>Output the user input in a h1 tag.</li>
-	<li>Color the output red (e.g. by adding a class) if it contains at least one exclamation mark.</li>
-	<li>Put the h1 tag + output into a separate component to which you pass the user input</li>
+	<li><strike>Add an input field that allows users to enter a course goal.</strike></li>
+	<li><strike>Output the user input in a h1 tag.</strike></li>
+	<li><strike>Color the output red (e.g. by adding a class) if it contains at least one exclamation mark</strike>.</li>
+	<li><strike>Put the h1 tag + output into a separate component to which you pass the user input</strike></li>
 </ol>
+
+<h2>Result</h2>
+
+<div>
+	<input bind:value={courseGoal} />
+
+	<CourseGoal {courseGoal} />
+</div>
---
+++ 02-assignment-problem/src/CourseGoal.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let courseGoal;
+</script>
+
+<div>
+	<h1 class:goal_highlight={courseGoal.includes('!')}>{courseGoal}</h1>
+</div>
+
+<style>
+	.goal_highlight {
+		color: red;
+	}
+</style>
